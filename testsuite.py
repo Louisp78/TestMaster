821c1efa08000b5b927158395a84f78ea24de05d
louis place <louis.place@epita.fr>
2022-03-09 19:58:44 +0100

    :sparkles: custom suite option

--- testsuite.py
+++ testsuite.py
@@ -1,4 +1,5 @@
 
+from asyncio import current_task
 from classes.testsuite import Testsuite
 from classes.bcolors import Bcolors
 import yaml
@@ -24,11 +25,12 @@ def pretty_print():
 pretty_print()
 
 argumentList = sys.argv[1:]
-options = "hvt:"
-long_options = ["help", "verbose", "timeout = "]
+options = "hvt:s:"
+long_options = ["help", "verbose", "timeout=", "suite="]
 stop = False
 timeout = 0.5
 verbose = False
+run_custom_testsuite = None
 try:
 
     arguments, values = getopt.getopt(argumentList, options, long_options)
@@ -39,18 +41,23 @@ try:
             help_msg = """
             -v or --verbose : Activate verbose mode to show commands executed
             -t <timeout in seconds> : Use a timeout of x seconds during tests
+            -s <name of testsuite> or --suite <name of testsuite> : Run only the specified testsuite
             -h or --help : For help
             """
             print (help_msg)
             stop = True
 
-        elif currentArgument in ("-v", "--verbose"):
+        if currentArgument in ("-v", "--verbose"):
             print (Bcolors.WARNING + Bcolors.BOLD + "[VERBOSE] " + Bcolors.ENDC + "Enabling verbose mode !\n")
             verbose = True
 
-        elif currentArgument in ("-t"):
+        if currentArgument in ("-t", "--timeout"):
             print ((Bcolors.WARNING + Bcolors.BOLD + "[TIMEOUT] " + Bcolors.ENDC + "Enabling timeout of %r seconds\n") % (currentValue))
             timeout = int(currentValue)
+
+        if currentArgument in ("--suite", "-s"):
+            print ((Bcolors.WARNING + Bcolors.BOLD + "[SUITE] " + Bcolors.ENDC + "Running only %r suite. \n") % (currentValue))
+            run_custom_testsuite = currentValue
 
 except getopt.error as err:
     print (str(err))
@@ -68,8 +75,13 @@ if not stop:
     total_tests = 0
     for map in list_map:
         testsuite = from_dict(data_class=Testsuite, data=map)
+        if run_custom_testsuite != None and testsuite.name == run_custom_testsuite:
+            (is_ok, nb_ok_tests) = testsuite.run(verbose, timeout)
+        elif run_custom_testsuite == None:
+            (is_ok, nb_ok_tests) = testsuite.run(verbose, timeout)
+        else:
+            continue
         list.append(testsuite)
-        (is_ok, nb_ok_tests) = testsuite.run(verbose, timeout)
         total_tests_ok += nb_ok_tests
         total_tests += len(testsuite.tests)
         if is_ok:
